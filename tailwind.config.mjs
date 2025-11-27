add36677f13f13475948b1d4a00a87dead3bbc32
aflamrip <112701602+aflamrip@users.noreply.github.com>
2025-11-27 15:06:57 +0200

    Refactor content paths in Tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,9 @@
-tailwind.config.mjs/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: [
+    './src/pages/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './src/components/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
   theme: {
     extend: {},
   },
